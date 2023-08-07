925919c1fdaa6d2f6005cb9be7a82a642d7ac15c
andrey <andrey.oveshkov@bk.ru>
2023-08-07 16:58:13 +0300

    Работа с админкой

--- movie_project/movie_app/admin.py
+++ movie_project/movie_app/admin.py
@@ -1,6 +1,71 @@
 from django.contrib import admin
+from django.db.models import QuerySet
 from .models import Movie
 
 # Register your models here.
 
-admin.site.register(Movie)
+class FilterRating(admin.SimpleListFilter):
+    title = 'Фильтр по рейтингу'
+    parameter_name = 'rating'
+
+    def lookups(self, request, model_admin):
+        return [
+            ('до 40', 'низкий'),
+            ('от 40 до 59', 'средний'),
+            ('от 60 до 79', 'высокий'),
+            ('от 80', 'высочайший'),
+        ]
+
+    def queryset(self, request, queryset: QuerySet):
+        rating = self.value()
+        if rating == 'до 40':
+            return queryset.filter(rating__lt=40)
+        if rating == 'от 40 до 59':
+            return queryset.filter(rating__gte=40).filter(rating__lt=59)
+        if rating == 'от 60 до 79':
+            return queryset.filter(rating__gte=60).filter(rating__lt=79)
+        if rating == 'от 80':
+            return queryset.filter(rating__gte=80)
+
+
+
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ['name', 'rating', 'year', 'currency', 'budget', 'rating_status']
+    list_editable = ['rating', 'year', 'currency']
+    ordering = ['-rating']
+    list_per_page = 5
+    actions = ['set_dollars', 'set_rubles']
+    search_fields = ['name__startswith', 'rating']
+    list_filter = ['name', 'currency', FilterRating]
+    exclude = ['slug']
+
+
+    @admin.display(ordering='rating', description='Статус')
+    def rating_status(self, movie: Movie):
+        if movie.rating < 50:
+            return 'Зачем это смотреть'
+        if movie.rating < 70:
+            return 'Можно разок глянуть'
+        if movie.rating <= 85:
+            return 'Зачет'
+        return 'Топчик'
+
+
+    @admin.action(description='Перевести валюту в доллар')
+    def set_dollars(self, requests, qs: QuerySet):
+        count_change = qs.update(currency=Movie.USD)
+        self.message_user(
+            requests,
+            f"Было обновлено {count_change} записей"
+        )
+
+
+    @admin.action(description='Перевести валюту в рубль')
+    def set_rubles(self, requests, qs: QuerySet):
+        count_change = qs.update(currency=Movie.RUB)
+        self.message_user(
+            requests,
+            f"Было обновлено {count_change} записей"
+        )
--- movie_project/movie_app/models.py
+++ movie_project/movie_app/models.py
@@ -1,13 +1,27 @@
 from django.db import models
 from django.urls import reverse
 from django.utils.text import slugify
+from django.core.validators import MaxValueValidator, MinValueValidator
+
 
 
 class Movie(models.Model):
+
+    EUR = 'EUR'
+    USD = 'USD'
+    RUB = 'RUB'
+
+    CURRENCY_CHOICES = [
+        (EUR, 'Euro'),
+        (USD, 'Dollars'),
+        (RUB, 'Rubles'),
+    ]
+
     name = models.CharField(max_length=40)
-    rating = models.IntegerField()
-    year = models.IntegerField(null=True)
-    budget = models.IntegerField(default=1000000)
+    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(100)])
+    year = models.IntegerField(null=True, blank=True)
+    currency = models.CharField(max_length=3, choices=CURRENCY_CHOICES, default=RUB)
+    budget = models.IntegerField(default=1000000, blank=True, validators=[MinValueValidator(1)])
     slug = models.SlugField(default='', null=False, db_index=True)
 
 
